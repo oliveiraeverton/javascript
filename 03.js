aeb34ac90be68c4ca5cc0282b547cbf41fdee01d
Everton de Oliveira <84155681+oliveiraeverton@users.noreply.github.com>
2022-06-24 10:24:44 -0400

    Create 03.js

---
+++ 03.js
@@ -0,0 +1,26 @@
+//boolean
+const usuarioLogado = true;
+const contaPaga = false;
+
+//truthy ou falsy
+
+//0 (false) - 1 (true)
+
+
+console.log(0 == false);//true
+console.log(0 === false);//false (compara valor e tipo)
+console.log(2 == true);//false
+console.log(1 == true);//true
+
+
+//null ==> vazio
+let a = null;
+//undefined ==>
+let b;
+
+console.log(a);
+console.log(b);
+
+
+console.log(typeof a);
+console.log(typeof b);
