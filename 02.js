8f019697da31c89c2183995d27d837f827c7abf2
Everton de Oliveira <84155681+oliveiraeverton@users.noreply.github.com>
2022-06-24 10:23:23 -0400

    Create 02.js

---
+++ 02.js
@@ -0,0 +1,30 @@
+//usando var
+
+var altura = 5;
+var comprimento = 7;
+area = altura * comprimento;
+console.log(area);
+var area;
+
+//PROVOCA UM ERRO
+/*tempo;
+console.log(tempo);*/
+
+//undefined
+console.log(teste);
+var teste;
+
+let area2;
+let forma = 'retângulo';
+if(forma === 'retângulo'){
+    area2 = altura * comprimento;
+}else{
+    area2 = altura * comprimento / 2;
+}
+console.log(area);
+
+let a;
+var b;
+console.log(a);//undefined
+console.log(b);//undefined
+
